4871803cfb4e549ab9359dbe86e35fcc8600b54b
AlfiTrm <grackrev@gmail.com>
2025-03-01 04:54:28 +0700

    feat: add public api

---
+++ src/api/api.ts
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://fakestoreapi.com';
+
+export const END_API = {
+    BASE_URL,
+    PRODUCTS: `${BASE_URL}/products`,
+    CARTS: `${BASE_URL}/carts`,
+    USER: `${BASE_URL}/users`,
+    LOGIN: `${BASE_URL}/auth/login`
+}
