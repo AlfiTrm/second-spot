49074eb5af8c1b12c4ed6eb85648c9064307e268
AlfiTrm <grackrev@gmail.com>
2025-03-07 13:40:58 +0700

    feat: add some route and import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './components/view/user/Home'
-import MainLayout from './layout/MainLayout'
-import Register from './components/view/auth/Register'
-import Login from './components/view/auth/Login'
-import Detail from './components/view/user/Detail'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//import layout
+import MainLayout from "./layout/MainLayout";
+
+// import user
+import Home from "./components/view/user/Home";
+import Detail from "./components/view/user/Detail";
+import Chat from "./components/view/user/Chat";
+import Favorite from "./components/view/user/Favorite";
+import Profile from "./components/view/user/Profile";
+import ChangeProfile from "./components/view/user/ChangeProfile";
+import Sell from "./components/view/user/Sell";
+
+// import auth
+import Register from "./components/view/auth/Register";
+import Login from "./components/view/auth/Login";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: (
       <MainLayout>
         <Home />
@@ -15,29 +26,68 @@ const router = createBrowserRouter([
     ),
   },
   {
-    path: '/products/:id',
+    path: "/products/:id",
     element: (
       <MainLayout>
-        <Detail id={0} title={''} price={0} category={''} description={''} image={''} />
+        <Detail
+          id={0}
+          title={""}
+          price={0}
+          category={""}
+          description={""}
+          image={""}
+        />
       </MainLayout>
     ),
   },
   {
-    path: '/Register',
+    path: "/chat",
+    element: <Chat />,
+  },
+  {
+    path: "/favorite",
     element: (
-        <Register id={0} username={''} email={''} password={''} />
+      <MainLayout>
+        <Favorite />
+      </MainLayout>
     ),
   },
   {
-    path: '/Login',
+    path: "/profile",
     element: (
-        <Login id={0} username={''} password={''} />
+      <MainLayout>
+        <Profile />
+      </MainLayout>
     ),
   },
-])
+  {
+    path: "/profile/change",
+    element: (
+      <MainLayout>
+        <ChangeProfile />
+      </MainLayout>
+    ),
+  },
+  {
+    path: "/sell",
+    element: (
+      <MainLayout>
+        <Sell />
+      </MainLayout>
+    ),
+  },
+  {
+    path: "/Register",
+    element: <Register />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
