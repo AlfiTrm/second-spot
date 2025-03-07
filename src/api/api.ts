def7832e37601f718967abd08732585fc6af638f
AlfiTrm <grackrev@gmail.com>
2025-03-07 13:56:48 +0700

    feat: add auth api from backend

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,14 @@
 const BASE_URL = 'https://fakestoreapi.com';
+const AUTH_URL = `https://be-intern.bccdev.id/arva`
 
 export const END_API = {
     BASE_URL,
     PRODUCTS: `${BASE_URL}/products`,
     CARTS: `${BASE_URL}/carts`,
     USER: `${BASE_URL}/users`,
-    LOGIN: `${BASE_URL}/auth/login`
+}
+
+export const END_AUTH = {
+    REGISTER : `${AUTH_URL}/api/register`,
+    LOGIN: `${AUTH_URL}/api/login`,
 }
