a694dbef30b7c26f564524e591e75269e2f82732
AlfiTrm <grackrev@gmail.com>
2025-03-07 14:27:43 +0700

    feat: add Chat page

---
+++ src/components/view/user/Chat.tsx
@@ -0,0 +1,12 @@
+import { MultiChatWindow } from "react-chat-engine-advanced";
+
+
+const Chat = () => {
+  return (
+    <div className="font-primary">
+        <MultiChatWindow style={{height:"100vh"}} />
+    </div>
+  );
+};
+
+export default Chat;
