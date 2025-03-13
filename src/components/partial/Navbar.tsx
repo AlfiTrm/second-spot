39d308f6875e6b6af286a3bcd240bf87de30d64e
AlfiTrm <grackrev@gmail.com>
2025-03-13 11:50:49 +0700

    feat: add some features

--- src/components/partial/Navbar.tsx
+++ src/components/partial/Navbar.tsx
@@ -4,15 +4,22 @@ import { HiMenu } from "react-icons/hi";
 import { BsChatLeftText } from "react-icons/bs";
 import { IoCloseOutline, IoNotificationsOutline } from "react-icons/io5";
 import { LuMinus, LuPlus, LuTrash, LuHeart } from "react-icons/lu";
+import logo from "../../assets/logo/logo.webp";
+import { useUser } from "@clerk/clerk-react";
 
 const Navbar: React.FC = () => {
+  const { user } = useUser();
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
   const navigate = useNavigate();
   return (
-    <nav className="fixed px-4 md:px-20 lg:px-32 h-20 top-0 flex font-semibold font-primary text-xl w-full justify-between z-50 p-6 bg-main border-b-gray-500 border-b">
-      <section className="flex items-center gap-20">
-        <div className="flex items-center text-lg">Logo</div>
+    <nav className="fixed px-10 md:px-14 2xl:px-33 h-20 top-0 flex font-semibold font-primary text-xl w-full justify-between z-50 p-6 bg-main border-b-gray-500 border-b">
+      <section className="flex items-center gap-9">
+        <img
+          onClick={() => navigate("/")}
+          src={logo}
+          className="flex items-center text-lg w-32 h-12 cursor-pointer"
+        ></img>
         <div className="hidden lg:flex gap-9 text-xl text-primary">
           <Link to={"/"}>Beranda</Link>
           <Link to={"/blabla"}>Kategori</Link>
@@ -21,65 +28,73 @@ const Navbar: React.FC = () => {
 
       <section className="flex items-center gap-5">
         <div className="flex items-center gap-5">
-          {/* {!user ? ( */}
-          <>
-            {/* <Link to={"/Register"}>
-              <button className="text-primary w-30 h-10 border border-primary rounded-full cursor-pointer">
-                Daftar
-              </button>
-            </Link>
-            <Link to={"/Login"}>
-              <button className="text-primary w-30 h-10 border border-primary rounded-full cursor-pointer">
-                Masuk
-              </button>
-            </Link>
-            <Link to={"/Login"}>
-              <button className="w-30 h-10 text-white bg-primary rounded-full cursor-pointer hover:bg-sky-700">
-                Jual
-              </button>
-            </Link> */}
-          </>
-          {/* ) : ( */}
-          <>
-            <div className="flex gap-6 text-primary">
-              <button onClick={() => navigate("/profile")} className="lg:flex items-center gap-1 hidden">
-                <div className="p-3 rounded-full bg-gray-300"></div>
-                <p className="text-xs">username</p>
-              </button>
+          {!user ? (
+            <>
+              <Link to={"/Register"}>
+                <button className="text-primary w-30 h-10 border border-primary rounded-full cursor-pointer">
+                  Daftar
+                </button>
+              </Link>
+              <Link to={"/Login"}>
+                <button className="text-primary w-30 h-10 border border-primary rounded-full cursor-pointer">
+                  Masuk
+                </button>
+              </Link>
+              <Link to={"/Login"}>
+                <button className="w-30 h-10 text-white bg-primary rounded-full cursor-pointer hover:bg-sky-700">
+                  Jual
+                </button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <div className="flex gap-6 text-primary">
+                <button
+                  onClick={() => navigate("/profile")}
+                  className="lg:flex items-center gap-1 hidden"
+                >
+                  <img
+                    src={user?.imageUrl}
+                    className="w-5 h-5 rounded-full bg-gray-300"
+                  ></img>
+                  <p className="text-xs">{user.username}</p>
+                </button>
 
-              <button onClick={() => navigate("/favorite")} className="cursor-pointer">
-                <LuHeart className="w-5 h-5" />
-              </button>
+                <button
+                  onClick={() => navigate("/favorite")}
+                  className="cursor-pointer"
+                >
+                  <LuHeart className="w-5 h-5" />
+                </button>
 
-              <button className="cursor-pointer">
-                <IoNotificationsOutline className="w-6 h-6" />
-              </button>
+                <button className="cursor-pointer">
+                  <IoNotificationsOutline className="w-6 h-6" />
+                </button>
 
-              <button className="cursor-pointer">
-                <BsChatLeftText className="w-5 h-5" />
-              </button>
+                <button className="cursor-pointer">
+                  <BsChatLeftText className="w-5 h-5" />
+                </button>
 
-              <button onClick={() => navigate("/sell")} className="w-30 h-10 text-white bg-primary rounded-full cursor-pointer hover:bg-sky-700">
-                Jual
-              </button>
-              <button
-                className="lg:hidden rounded-lg cursor-pointer"
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-              >
-                <HiMenu className="text-primary w-6 h-6" />
-              </button>
-            </div>
-
-            <div className="text-black cursor-pointer">
-              {/* <CgProfile className="h-6 w-6" /> */}
-            </div>
-          </>
-          {/* )} */}
+                <button
+                  onClick={() => navigate("/sell")}
+                  className="w-30 h-10 text-white bg-primary rounded-full cursor-pointer hover:bg-sky-700"
+                >
+                  Jual
+                </button>
+                <button
+                  className="lg:hidden rounded-lg cursor-pointer"
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                  <HiMenu className="text-primary w-6 h-6" />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </section>
 
       {isMenuOpen && (
-        <div className="absolute top-full left-0 w-full h-[100vh] bg-white lg:hidden shadow-lg">
+        <div className="absolute top-full left-0 w-full h-lvh bg-white lg:hidden shadow-lg">
           <div className="flex flex-col p-4 gap-5 items-center">
             <Link to={"/"} className="text-primary">
               Beranda
