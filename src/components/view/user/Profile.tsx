37c918eb486533b02d5873d493b00b080dedc262
AlfiTrm <grackrev@gmail.com>
2025-03-13 11:20:00 +0700

    feat: add some features

--- src/components/view/user/Profile.tsx
+++ src/components/view/user/Profile.tsx
@@ -1,12 +1,27 @@
+import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
-
+import { IProduct } from "../../../data/Type";
+import ProductCards from "../../partial/home/ProductCards";
 
 const Profile = () => {
-    const navigate = useNavigate();
-  
-    return (
-    <div>
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const [product, setProduct] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    const allProduct = JSON.parse(localStorage.getItem("products") || "[]");
+    const userProduct = allProduct.filter(
+      (product: IProduct) => product.userId === user?.id
+    );
+    console.log("Semua data", allProduct);
+    console.log("Data Filter:", userProduct);
+    setProduct(userProduct);
+  }, [user?.id]);
+
+  return (
+    <div className="md:w-full sm:w-full w-lvh overflow-hidden">
       <section className="mt-25 flex gap-5 justify-center items-center">
         <form action="" className="relative">
           <input
@@ -35,12 +50,15 @@ const Profile = () => {
       <section className="flex justify-center items-center mt-5">
         <div className="w-295 h-50 bg-main flex items-center justify-between px-5 rounded-2xl">
           <figure className="flex gap-5 items-center">
-            <div className="w-25 h-z25 bg-gray-400 rounded-full"></div>
+            <img src={user?.imageUrl} className="w-25 h-z25 rounded-full" />
             <figcaption className="text-4xl font-semibold text-gray">
-              Username
+              {user?.username}
             </figcaption>
           </figure>
-          <button onClick={() => navigate("change")} className="w-40 h-10 bg-primary text-white rounded-full cursor-pointer hover:bg-sky-700">
+          <button
+            onClick={() => navigate("changeProfile")}
+            className="w-40 h-10 bg-primary text-white rounded-full cursor-pointer hover:bg-sky-700"
+          >
             Ubah Profil
           </button>
         </div>
@@ -48,14 +66,22 @@ const Profile = () => {
 
       <section className="mt-5">
         <header className="flex justify-center text-primary font-semibold text-4xl">
-            <h2>Produk</h2>
+          <h2>Produk</h2>
         </header>
+        <section className=" bg-main pt-8 px-16 2xl:px-32 flex justify-center items-center mt-10">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5">
+            {product.length > 0 ? (
+              product.map((product) => (
+                <ProductCards key={product.id} {...product} />
+              ))
+            ) : (
+              <p className="text-center w-full  text-red-600 mt-5">
+                Belum ada produk yang dijual
+              </p>
+            )}
+          </div>
+        </section>
       </section>
-
-
-
-    
-
     </div>
   );
 };
