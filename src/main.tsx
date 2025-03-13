9a92e424007f1f5e835299958f8e623626a01a01
AlfiTrm <grackrev@gmail.com>
2025-03-13 11:58:30 +0700

    feat: add ClerkProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const PUBLISHABLE_KEY = "pk_test_Z2VudGxlLWNyYXlmaXNoLTk2LmNsZXJrLmFjY291bnRzLmRldiQ";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ClerkProvider>
+);
