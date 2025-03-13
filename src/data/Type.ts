432be2ee9e8ce3bfa976d89b797e2843253acc1f
AlfiTrm <grackrev@gmail.com>
2025-03-13 11:36:28 +0700

    chore: export interface and type

---
+++ src/data/Type.ts
@@ -0,0 +1,20 @@
+export interface IProduct {
+  [x: string]: any;
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  location?: string;
+  condition?: string;
+  userId: string;
+}
+
+export type UserProps = {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+};
