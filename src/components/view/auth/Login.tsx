983d553e33e838317874d748911e827338646900
AlfiTrm <grackrev@gmail.com>
2025-03-13 11:16:13 +0700

    feat: integrated with clerk api

--- src/components/view/auth/Login.tsx
+++ src/components/view/auth/Login.tsx
@@ -1,28 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FormEvent, useState } from "react";
 import { BiHide, BiShowAlt } from "react-icons/bi";
-import { END_AUTH } from "../../../api/api";
-import axios from "axios";
-
-type LoginProps = {
-  identifier: string;
-  password: string;
-};
+import logo from "../../../assets/logo/logo.webp";
+import { useSignIn } from "@clerk/clerk-react";
 
 const Login = () => {
+  const { signIn } = useSignIn();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [user, setUser] = useState<LoginProps>({} as LoginProps);
 
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const credentials = {
-      identifier: user.identifier,
-      password: user.password,
-    };
+
     try {
-      const response = await axios.post(`${END_AUTH.LOGIN}`, credentials);
-      const data = await response.data;
-      console.log(data);
+      const result = await signIn?.create({
+        identifier: email,
+        password,
+      });
+
+      if (result?.status === "complete") {
+        navigate("/");
+      } else {
+        alert("Login gagal");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -30,47 +34,37 @@ const Login = () => {
 
   return (
     <div>
-      <div className="font-primary w-full h-lvh bg-white">
-        <div className="flex lg:justify-between justify-center items-center h-full lg:px-52 2xl:px-56">
-          <section className="text-primary border min-w-4/12 md:w-6/12 lg:w-6/12 lg:flex hidden md:visible flex-col gap-10">
-            <div className="flex justify-center">ini logo</div>
-            {/* <p>
-              Second Spot adalah platform jual beli barang bekas yang dirancang
-              khusus untuk mahasiswa dan masyarakat di Malang. Dengan fokus pada
-              kemudahan, keamanan, dan transparansi transaksi, Second Spot
-              menjadi solusi bagi mereka yang ingin menjual atau membeli barang
-              bekas dengan lebih nyaman dan terpercaya.
-            </p> */}
-          </section>
+      <div className="font-primary w-full h-lvh bg-white flex">
+        <section className=" bg-white w-full md:flex justify-center items-center h-full hidden">
+          <figure className="text-primary min-w-4/12 md:w-6/12 ">
+            <img src={logo} alt="" />
+          </figure>
+        </section>
 
-          <section className="w-110 h-150 lg:w-120 2xl:h-150 bg-white rounded-xl shadow-2xl shadow-gray-500">
+        <section className="w-full bg-primary flex items-center justify-center">
+          <section className="w-90 h-150 2xl:w-120 2xl:h-150 bg-white rounded-xl shadow-2xl shadow-gray-500">
             <div className="px-8 pt-35 flex flex-col gap-8">
-              <div className="font-semibold text-primary lg:text-3xl 2xl:text-4xl">
+              <div className="font-semibold text-primary text-3xl 2xl:text-4xl">
                 <h2>Masuk</h2>
               </div>
               <form onSubmit={handleLogin}>
                 <div className="w-full flex flex-col gap-8">
                   <input
-                    value={user.identifier}
-                    onChange={(e) =>
-                      setUser({ ...user, identifier: e.target.value })
-                    }
+                    value={email}
                     type="email"
                     placeholder="email"
+                    onChange={(e) => setEmail(e.target.value)}
                     required
-                    className="shadow shadow-primary/50  w-full h-10 px-3 rounded-full"
+                    className="w-full h-10 px-3 rounded-full border border-primary"
                   />
                   <div className="relative">
                     <input
-                      value={user.password}
-                      onChange={(e) =>
-                        setUser({ ...user, password: e.target.value })
-                      }
-                      autoComplete="on"
+                      value={password}
                       type={showPassword ? "text" : "password"}
                       placeholder="password"
+                      onChange={(e) => setPassword(e.target.value)}
                       required
-                      className="shadow shadow-primary/50 w-full h-10 px-3 rounded-full"
+                      className="w-full h-10 px-3 rounded-full border border-primary"
                     />
                     <button
                       type="button"
@@ -84,16 +78,17 @@ const Login = () => {
                       )}
                     </button>
                   </div>
+
                   <div className="flex flex-col items-center gap-8">
                     <button
                       type="submit"
-                      className="flex justify-center cursor-pointer bg-primary text-white font-semibold items-center w-1/2 h-11 shadow shadow-gray-500 rounded-full"
+                      className="flex justify-center cursor-pointer bg-primary text-white font-semibold items-center w-1/2 h-10 shadow shadow-gray-500 rounded-full hover:bg-sky-600"
                     >
                       <h3>MASUK</h3>
                     </button>
                     <p>
-                      Sudah punya akun?
-                      <span className="text-primary cursor-pointer">
+                      Belum punya akun?
+                      <span className="text-primary cursor-pointer font-semibold">
                         <Link to={"/Register"}> Daftar</Link>
                       </span>
                     </p>
@@ -102,7 +97,7 @@ const Login = () => {
               </form>
             </div>
           </section>
-        </div>
+        </section>
       </div>
     </div>
   );
