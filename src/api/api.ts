a29f2b7f5fafa2d0d54b5338ef001ecefeb12032
AlfiTrm <grackrev@gmail.com>
2025-03-13 11:08:52 +0700

    chore: remove unused api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,8 @@
 const BASE_URL = 'https://fakestoreapi.com';
-const AUTH_URL = `https://be-intern.bccdev.id/arva`
 
 export const END_API = {
     BASE_URL,
     PRODUCTS: `${BASE_URL}/products`,
     CARTS: `${BASE_URL}/carts`,
     USER: `${BASE_URL}/users`,
-}
-
-export const END_AUTH = {
-    REGISTER : `${AUTH_URL}/api/register`,
-    LOGIN: `${AUTH_URL}/api/login`,
 }
