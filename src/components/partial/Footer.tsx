326d90289e90bd32fb4443906eecf944e4e7f476
AlfiTrm <grackrev@gmail.com>
2025-03-13 11:51:38 +0700

    feat: add some features

--- src/components/partial/Footer.tsx
+++ src/components/partial/Footer.tsx
@@ -1,23 +1,28 @@
+import logo from "../../assets/logo/logo.webp";
 
 const Footer = () => {
   return (
-    <div className="">
-      <div className="flex justify-center border-t bg-white text-primary w-full h-96 gap-20 ">
-        <section className="w-md ml-48 mt-24">
-          <img src="" alt="" />
-          <p className="font-medium text-md">
-            <span className="font-bold">“Second Spot”</span> adalah platform
-            jual beli barang bekas yang dirancang khusus untuk mahasiswa di
-            Malang. Dengan fokus pada kemudahan, keamanan, dan transparansi
-            transaksi, Second Spot menjadi solusi bagi mereka yang ingin menjual
-            atau membeli barang bekas dengan lebih nyaman dan terpercaya.
+    <div className=" md:w-full sm:w-full w-lvh overflow-hidden">
+      <div className="flex flex-col md:flex-row bg-white border-t text-primary sm:w-full gap-10 md:gap-20 p-4">
+        <section className="w-full md:w-md 2xl:ml-48 mt-24 flex md:flex-col flex-row">
+          <img src={logo} alt="" className="w-64 h-18" />
+          <p className="font-medium text-base text-gray">
+            <span className="font-bold text-primary">“Second Spot”</span> adalah
+            platform jual beli barang bekas yang dirancang khusus untuk
+            mahasiswa di Malang. Dengan fokus pada kemudahan, keamanan, dan
+            transparansi transaksi, Second Spot menjadi solusi bagi mereka yang
+            ingin menjual atau membeli barang bekas dengan lebih nyaman dan
+            terpercaya.
           </p>
         </section>
 
         <section className="flex gap-20 mt-16">
-          <div className="w-52">
-            <h3 className="font-medium text-xl">Kategori</h3>
-            <div className="font-normal mt-0.5 text-lg">
+          <div className="w-full md:w-52">
+            <h3 className="font-bold text-xl text-center md:text-left">
+              Kategori
+            </h3>
+
+            <div className="font-medium mt-0.5 text-lg text-gray ">
               <ul>
                 <li>
                   <a href="">Buku & Alat Tulis</a>
@@ -44,11 +49,11 @@ const Footer = () => {
             </div>
           </div>
           <section>
-            <h3 className="font-medium text-xl">Ikuti Kami</h3>
-            <div className="flex gap-5 mt-2.5">
-              <div className="p-8 bg-white rounded-full"></div>
-              <div className="p-8 bg-white rounded-full"></div>
-              <div className="p-8 bg-white rounded-full"></div>
+            <h3 className="font-bold text-xl">Ikuti Kami</h3>
+            <div className="flex flex-col md:flex-row gap-5 mt-2.5">
+              <div className="p-8 bg-gray-300 rounded-full"></div>
+              <div className="p-8 bg-gray-300 rounded-full"></div>
+              <div className="p-8 bg-gray-300 rounded-full"></div>
             </div>
           </section>
         </section>
