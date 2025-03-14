77c025d3eba92c4a8bdcd0d84e4af0bbe08bf243
AlfiTrm <grackrev@gmail.com>
2025-03-15 05:39:58 +0700

    chore: delete unused components

--- src/components/view/user/Profile.tsx
+++ src/components/view/user/Profile.tsx
@@ -1,9 +1,9 @@
 import { useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
-import { FiSearch } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { IProduct } from "../../../data/Type";
 import ProductCards from "../../partial/home/ProductCards";
+import supabase from "../../../utils/supabase";
 
 const Profile = () => {
   const { user } = useUser();
@@ -11,43 +11,25 @@ const Profile = () => {
   const [product, setProduct] = useState<IProduct[]>([]);
 
   useEffect(() => {
-    const allProduct = JSON.parse(localStorage.getItem("products") || "[]");
-    const userProduct = allProduct.filter(
-      (product: IProduct) => product.userId === user?.id
-    );
-    console.log("Semua data", allProduct);
-    console.log("Data Filter:", userProduct);
-    setProduct(userProduct);
+    const fetchUserProduct = async () => {
+      const { data, error } = await supabase
+        .from("products")
+        .select("*")
+        .eq("userId", user?.id);
+
+      if (error) {
+        console.error("Gagal mengambil data produk:", error.message);
+        setProduct([]);
+      } else {
+        setProduct(data || []);
+      }
+    };
+    fetchUserProduct();
   }, [user?.id]);
 
   return (
     <div className="md:w-full sm:w-full w-lvh overflow-hidden">
-      <section className="mt-25 flex gap-5 justify-center items-center">
-        <form action="" className="relative">
-          <input
-            type="search"
-            name=""
-            id=""
-            placeholder="Cari Produk"
-            className="w-195 h-12 px-5 border border-gray-300 bg-white shadow-gray-200 rounded-full"
-          />
-          <button className="cursor-pointer">
-            <div className="w-20 h-8 bg-primary absolute top-2 right-5 rounded-full"></div>
-            <FiSearch className="absolute text-white w-4 h-4 top-4 right-13" />
-          </button>
-        </form>
-
-        <form className="w-95 h-12">
-          <select className="w-full h-full px-2.5 border bg-white border-gray-300 rounded-full">
-            <option selected>Pilih lokasi</option>
-            <option value="1">Sukun</option>
-            <option value="2">Dau</option>
-            <option value="3">Ngawi</option>
-          </select>
-        </form>
-      </section>
-
-      <section className="flex justify-center items-center mt-5">
+      <section className="flex justify-center items-center mt-30">
         <div className="w-295 h-50 bg-main flex items-center justify-between px-5 rounded-2xl">
           <figure className="flex gap-5 items-center">
             <img src={user?.imageUrl} className="w-25 h-z25 rounded-full" />
@@ -69,7 +51,7 @@ const Profile = () => {
           <h2>Produk</h2>
         </header>
         <section className=" bg-main pt-8 px-16 2xl:px-32 flex justify-center items-center mt-10">
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5 mb-5">
             {product.length > 0 ? (
               product.map((product) => (
                 <ProductCards key={product.id} {...product} />
